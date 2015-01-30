57aa80526b176a65d1271dc95ad9b252d37d498e
jiangchuan <jiangchuanheglobal@gmail.com>
2015-01-30 15:41:07 -0500

    WRCA Server program v0.1

--- install.sh
+++ install.sh
@@ -2,7 +2,15 @@
 
 # install on ubuntu 12.04
 
-read -p 'Are you sure to install on ubuntu:' opt
+read -p 'Are you sure to install on ubuntu[y/n]:' option
 
+if [ $option != 'y' ];
+then
+    return;
+fi
+
+# do install
 mv index.php index.php_bak
 mv index_ubuntu.php index.php
+
+mv ../wrca_server ../v1
